5d7267d2c7b3e293d7f591bff89f0fd3509556ee
drazenp <drazenp86@gmail.com>
2018-02-06 22:51:21 +0000

    Fix tests; Add failing test

--- test/headercalspan.test.js
+++ test/headercalspan.test.js
@@ -4,19 +4,18 @@ var jsdom = require("jsdom");
 var { document } = (new jsdom.JSDOM('<!DOCTYPE html><body></body></html>')).window;
 var fs = require('fs');
 
-var _table = fs.readFileSync('./test/plaintable.html', 'utf8');
+var _table = fs.readFileSync('./test/headercalspan.html', 'utf8');
 
-describe('toPlainTable', function() {
-    describe('Plain table', function() {
-        beforeEach(function () {
-            document.body.innerHTML = _table;
-        });
+describe('toPlainTable', function () {
+    beforeEach(function () {
+        document.body.innerHTML = _table;
+    });
 
-        it('already has equal number of cells in rows', function() {
+    describe('table with calspan 2 in header', function () {
+        it('has equal number of cells in the rows', function () {
             var rows = document.querySelectorAll('tr');
             assert.equal(rows[0].querySelectorAll('th').length, 3);
             assert.equal(rows[1].querySelectorAll('td').length, 3);
-            assert.equal(rows[2].querySelectorAll('td').length, 3);
         });
     });
 });
