ee15acd9cc4d5391678addaa65f5910bbc07de70
drazenp <drazenp86@gmail.com>
2018-02-06 21:59:01 +0000

    Created test for plain table

--- test/plaintable.test.js
+++ test/plaintable.test.js
@@ -1,10 +1,17 @@
 var toPlainTable = require('../src/index');
 var chai = require('chai');
+var jsdom = require("jsdom");
+var { JSDOM } = jsdom;
+var fs = require('fs');
+
+var _table = JSDOM.fragment(fs.readFile('./test/plaintable.html'), 'utf8');
 
 describe('toPlainTable', function() {
     describe('Plain table', function() {
-        it('', function() {
-
+        it('already has equal number of cells in rows', function() {
+            _table.querySelectorAll('tr').forEach(row => {
+                assert.equal(row.querySelectorAll(td), 3);
+            });
         });
     });
 });
