9654faa63e6b069b4c1bea0ea024747b14ea3c3f
drazenp <drazenp86@gmail.com>
2018-02-07 21:13:04 +0000

    Added more failing tests for colspan

--- test/headercalspan.test.js
+++ test/headercalspan.test.js
@@ -17,5 +17,22 @@ describe('toPlainTable', function () {
             assert.equal(rows[0].querySelectorAll('th').length, 3);
             assert.equal(rows[1].querySelectorAll('td').length, 3);
         });
+        it('cells have no rowspan or calspan attributes', function () {
+            var rows = document.querySelectorAll('tr');
+            rows[0].querySelectorAll('th').forEach(th => {
+                assert.equal(th.hasAttribute('colspan'), false);
+                assert.equal(th.hasAttribute('rowspan'), false);
+            });
+            rows[1].querySelectorAll('td').forEach(td => {
+                assert.equal(td.hasAttribute('colspan'), false);
+                assert.equal(td.hasAttribute('rowspan'), false);
+            });
+        });
+        it('colspan content is replicated', function () {
+            var headerCells = document.querySelectorAll('tr')[0].querySelectorAll('th');
+            assert.equal(headerCells[0].innerHTML, '1');
+            assert.equal(headerCells[1].innerHTML, '23');
+            assert.equal(headerCells[2].innerHTML, '23');
+        });
     });
 });
--- test/plaintable.test.js
+++ test/plaintable.test.js
@@ -6,17 +6,31 @@ var fs = require('fs');
 
 var _table = fs.readFileSync('./test/plaintable.html', 'utf8');
 
-describe('toPlainTable', function() {
-    describe('Plain table', function() {
+describe('toPlainTable', function () {
+    describe('Plain table', function () {
         beforeEach(function () {
             document.body.innerHTML = _table;
         });
 
-        it('already has equal number of cells in rows', function() {
+        it('has equal number of cells in rows', function () {
             var rows = document.querySelectorAll('tr');
             assert.equal(rows[0].querySelectorAll('th').length, 3);
             assert.equal(rows[1].querySelectorAll('td').length, 3);
             assert.equal(rows[2].querySelectorAll('td').length, 3);
+        });
+
+        it('cells have no rowspan or calspan attributes', function () {
+            var rows = document.querySelectorAll('tr');
+            rows[0].querySelectorAll('th').forEach(th => {
+                assert.equal(th.hasAttribute('colspan'), false);
+                assert.equal(th.hasAttribute('rowspan'), false);
+            });
+            for (var i = 1; i < rows.length; i++) {
+                rows[i].querySelectorAll('td').forEach(td => {
+                    assert.equal(td.hasAttribute('colspan'), false);
+                    assert.equal(td.hasAttribute('rowspan'), false);
+                });
+            }
         });
     });
 });
