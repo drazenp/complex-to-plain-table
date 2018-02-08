ba593e482ef52b1f26f60fdc56fd3b457cc0a74e
drazenp <drazenp86@gmail.com>
2018-02-08 22:15:10 +0000

    Clone cells for any number in colspan in header

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
-var toPlainTable = function(element) {
-    var ths = element.getElementsByTagName('th');
-    for(var i = 0; i < ths.length; i++) {
-        var th = ths[i];
-        if(th.hasAttribute('colspan')) {
+
+var cloneCell = function (cell, colspan) {
+    var parent = cell.parentNode;
+    var appendToParent = (parent.lastChild == cell);
+    
+    for (var i = 1; i < colspan; i++) {
+        var newCell = cell.cloneNode(true);
+        if(appendToParent) {
+            parent.appendChild(newCell);
+        } else {
+            parent.insertBefore(newCell, cell.nextSibling);
+        }
+    }
+};
+
+var toPlainTable = function (element) {
+    var headerCells = element.getElementsByTagName('th');
+    for (var i = 0; i < headerCells.length; i++) {
+        var th = headerCells[i];
+        if (th.hasAttribute('colspan')) {
+            var colspan = th.getAttribute('colspan');
             th.removeAttribute('colspan');
-            var newTh = th.cloneNode(true);
-            th.parentElement.insertBefore(newTh, th.nextSibling);
+            cloneCell(th, colspan);
         }
     }
 };
