f1578707876e26be399a22f118f589ff420a3163
drazenp <drazenp86@gmail.com>
2018-02-15 23:00:11 +0000

    Add uglify; support for javascript; lint

--- src/index.js
+++ src/index.js
@@ -1,39 +1,57 @@
+(function () {
+    'use strict';
+    var toPlainTable = (function () {
+        var toPlainTable = function (element) {
+            _splitCells(element, 'colspan', cloneCellToRight);
+            _splitCells(element, 'rowspan', cloneCellDownRows);
+        };
 
-var cloneCellToRight = function (cell, colspan) {
-    var parent = cell.parentElement;
-    
-    for (var i = 1; i < colspan; i++) {
-        var newCell = cell.cloneNode(true);
-        parent.insertBefore(newCell, cell.nextSibling);
+        var _splitCells = function (element, spanAttribute, cloneCell) {
+            var cellsWithSpanAttribute = element.querySelectorAll('[' + spanAttribute + ']');
+            for (var i = 0; i < cellsWithSpanAttribute.length; i++) {
+                var cell = cellsWithSpanAttribute[i];
+                if (cell.hasAttribute(spanAttribute)) {
+                    var count = cell.getAttribute(spanAttribute);
+                    cell.removeAttribute(spanAttribute);
+                    cloneCell(cell, +count);
+                }
+            }
+        };
+
+        var cloneCellToRight = function (cell, colspan) {
+            var parent = cell.parentElement;
+
+            for (var i = 1; i < colspan; i++) {
+                var newCell = cell.cloneNode(true);
+                parent.insertBefore(newCell, cell.nextSibling);
+            }
+        };
+
+        var cloneCellDownRows = function (cell, count) {
+            if (count < 2) return;
+
+            var nextRow = cell.parentElement.nextElementSibling;
+            var nextCell = nextRow.cells[cell.cellIndex];
+
+            var newCell = cell.cloneNode(true);
+            nextRow.insertBefore(newCell, nextCell);
+            cloneCellDownRows(newCell, --count);
+        };
+
+        return toPlainTable;
+    })();
+
+    if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+        module.exports = toPlainTable;
     }
-};
-
-var cloneCellDownRows = function(cell, count) {
-    if (count < 2) return;
-
-    var nextRow = cell.parentElement.nextElementSibling;
-    var nextCell = nextRow.cells[cell.cellIndex];
-
-    var newCell = cell.cloneNode(true);
-    nextRow.insertBefore(newCell, nextCell);
-    cloneCellDownRows(newCell, --count);
-};
-
-var splitCells = function(element, spanAttribute, cloneCell) {
-    var cellsWithSpanAttribute = element.querySelectorAll('[' + spanAttribute +']');
-    for (var i = 0; i < cellsWithSpanAttribute.length; i++) {
-        var cell = cellsWithSpanAttribute[i];
-        if (cell.hasAttribute(spanAttribute)) {
-            var count = cell.getAttribute(spanAttribute);
-            cell.removeAttribute(spanAttribute);
-            cloneCell(cell, +count);
+    else {
+        if (typeof define === 'function' && define.amd) {
+            define([], function () {
+                return toPlainTable;
+            });
+        }
+        else {
+            window.toPlainTable = toPlainTable;
         }
     }
-};
-
-var toPlainTable = function (element) {
-    splitCells(element, 'colspan', cloneCellToRight);
-    splitCells(element, 'rowspan', cloneCellDownRows);
-};
-
-module.exports = toPlainTable;
+})();
